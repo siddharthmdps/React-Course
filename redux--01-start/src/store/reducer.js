1a7942401c9e26dda94dd31b0acd7c1f66243a23
Siddharth SIngh <siddharthmdps@gmail.com>
2019-12-07 19:50:45 +0800

    Redux practise

---
+++ redux--01-start/src/store/reducer.js
@@ -0,0 +1,28 @@
+const initialState = {
+    counter: 0
+}
+
+const reducer = (state = initialState, action) => {
+    switch(action.type){
+        case 'INCREMENT':
+            return {
+                counter: state.counter+1
+            }
+        case 'DECREMENT':
+            return {
+                counter: state.counter-1
+            }
+        case 'ADD':
+            return {
+                counter: state.counter+action.payload.value
+            }
+        case 'SUBTRACT':
+            return {
+                counter: state.counter-action.payload.value
+            }
+        default:
+            return state;    
+    }
+}
+
+export default reducer;
